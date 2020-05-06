55aa5280df1c2cb99cc60b1660da0fe1cc73d2e4
Mani <Imani.Beasley.72@gmail,com>
2020-05-05 23:15:57 -0500

    more progress

---
+++ Full-data-alchemy/my_mod.py
@@ -0,0 +1,21 @@
+# my_other_alchemy.py
+
+
+
+def enlarge(n):
+    return n * 100
+
+
+# x = 5 
+# print(enlarge(x))
+
+
+if __name__ == '__main__':
+    # only run the code below ix exe code from cmd line
+    #otherwise don;t run it()
+
+
+
+    y = int(input('please choose a number like 3:'))
+
+    print(enlarge(y))
---
+++ Full-data-alchemy/my_script.py
@@ -0,0 +1,15 @@
+#my_alchemy.py
+
+import pandas as pd
+
+from my_mod import enlarge
+
+
+
+print("Yeet")
+
+df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
+
+
+x = 11
+print(enlarge(x))
