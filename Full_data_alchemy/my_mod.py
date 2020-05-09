bd61bb6d5343e1f098a935fdabce8dc48c271507
Mani <Imani.Beasley.72@gmail,com>
2020-05-09 12:30:07 -0500

    huh

--- Full_data_alchemy/my_mod.py
+++ Full_data_alchemy/my_mod.py
@@ -1,6 +1,6 @@
 # Full_data_alchemymy_mod.py
 
-# import shap
+import shap
 from sklearn.model_selection import train_test_split
 
 def enlarge(n):
@@ -13,31 +13,26 @@ def splitter(df):
     y = test
     return X, y
 
-# def shap_val(row, model, encoder):
-#     row = X_test.loc[[row]]
-#     row
+def shap_val(row, model, encoder):
+    row = X_test.loc[[row]]
+    row
 
-#     explainer = shap.TreeExplainer(model)
-#     encoder = encoder
-#     row_processed = encoder.transform(row)
-#     shap_values = explainer.shap_values(row_processed)
+    explainer = shap.TreeExplainer(model)
+    encoder = encoder
+    row_processed = encoder.transform(row)
+    shap_values = explainer.shap_values(row_processed)
 
-#     shap.initjs()
-#     return shap.force_plot(
-#         base_value=explainer.expected_value,
-#         shap_values=shap_values,
-#         features=row,
-#         link='logit'
-
-# x = 5 
-# print(enlarge(x))
+    shap.initjs()
+    return shap.force_plot(
+        base_value=explainer.expected_value,
+        shap_values=shap_values,
+        features=row,
+        link='logit')
 
 
 if __name__ == '__main__':
     # only run the code below ix exe code from cmd line
     #otherwise don;t run it()
-
-
 
     y = int(input('please choose a number like 3:'))
 
